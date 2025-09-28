6bcc72243ecd29afa20d6a804ecaee9396dbc467
Glawther <glawthers@gmail.com>
2025-09-28 12:58:23 -0300

    SECURE: Removidas chaves API do codigo-fonte

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -4,19 +4,16 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
-// CHAVES DE CONEXÃO OBTIDAS DO CONSOLE DO FIREBASE
+// NOVO: Lê as variáveis do arquivo .env usando o padrão do VITE (import.meta.env)
 const firebaseConfig = {
-  apiKey: "AIzaSyA42o4lLIDB_B5dS1gESGO5LKelwT-ywZo",
-  authDomain: "nosso-cantinho-app-334df.firebaseapp.com",
-  projectId: "nosso-cantinho-app-334df",
-  storageBucket: "nosso-cantinho-app-334df.firebasestorage.app",
-  messagingSenderId: "787758664563",
-  appId: "1:787758664563:web:9991efbcd7888045385306"
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID
 };
 
-// Inicializa o Firebase
 const app = initializeApp(firebaseConfig);
-
-// Exporta o Firestore (Banco de Dados) para que o React possa usá-lo
 export const db = getFirestore(app);
 export const auth = getAuth(app);
